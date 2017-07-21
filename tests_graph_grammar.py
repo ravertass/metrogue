a8c971b8d8e160660867a7ac3097d178f9ef58f1
Fabian Sörensson <fabian.sorensson@gmail.com>
2017-07-22 00:17:55 +0200

    Things are kind of working... kind of

---
+++ tests_graph_grammar.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python2
+
+import unittest
+
+class GraphTests(unittest.TestCase):
+    def test_node(self):
+        pass
+
+    def test_tree(self):
+        pass
+
+    def test_cycle(self):
+        pass
+
+    def test_is_construction(self):
+        pass
+
+    def test_basic_replace_construction(self):
+        pass
+
+    def test_complex_replace_construction(self):
+        pass
+
+    def test_incorrect_replace_construction(self):
+        pass
+
+class GrammarTests(unittest.TestCase):
+    def test_basic_rule(self):
+        pass
+
+    def test_complex_rule(self):
+        pass
+
+    def test_basic_grammar(self):
+        pass
+
+    def test_complex_grammar(self):
+        pass
+
+if __name__ == '__main__'
+
